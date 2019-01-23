69b6680f53afb3ab88a9320261e8bc66d7341d49
ko kristin <koberlander115@gmail.com>
2019-01-23 15:49:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function appendKitten(name){
 
 function prependKitten(name){
   //prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
-  var prependKittens = 
+  var prependKittens = kittens.unshift("Arnold");
   return prependKittens;
 }
 
