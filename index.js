5cce100f1d590ba77d18c4245df26aee12ddd7f8
ko kristin <koberlander115@gmail.com>
2019-01-23 15:10:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ function removeLastKitten(){
 function removeFirstKitten(){
   //removes the first kitten from the kittens arrayand returns a new array, leaving the kittens array unchanged
   
+  
 }
 
 
